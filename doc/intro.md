3453ce5aac6f2d0cea505efe0c61e4cbc2957a50
garanjones <garanjones@riverford.co.uk>
2017-05-22 21:29:24 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to virtual_parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/virtual_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns virtual-parser.core-test
+  (:require [clojure.test :refer :all]
+            [virtual-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
