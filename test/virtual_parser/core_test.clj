8510ea223f3e3a1c8583be569b5c13a98a715adf
garanjones <pasted@users.noreply.github.com>
2017-06-30 11:17:30 +0100

    wip slurping text file and tests in place

--- test/virtual_parser/core_test.clj
+++ test/virtual_parser/core_test.clj
@@ -1,6 +1,6 @@
 (ns virtual-parser.core-test
   (:require [clojure.test :refer :all]
-            [virtual-parser.core :refer :all]))
+            [virtual_parser.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
